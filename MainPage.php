bb292ea85330ea8c79252614d2b0236f796889a2
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2019-10-14 15:26:29 +0100

    Initialise PHP main page

---
+++ MainPage.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+    <html>
+    <title> Assessment </title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <!-- imports third party font -->
+    <link rel="stylesheet" href="mystyle.css">
+    <!-- import style sheet -->
+    <html lang="en" dir="LTR">
+
+    <head>
+    </head>
+
+    <header>
+    </header>
+
+    <nav>
+        <!-- nav bar -->
+        <ul>
+            <li>
+            </li>
+        </ul>
+    </nav>
+
+    <body>
+        <!-- signifies body of the page -->
+
+        <footer>
+        </footer>
+
+    </html>
