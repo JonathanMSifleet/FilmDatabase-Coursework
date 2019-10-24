eb6a9e2c61b56af25fd68a60aea5b713e9ae59b8
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2019-10-24 17:08:56 +0100

    Add code for IMDB image getting

--- MainPage.php
+++ MainPage.php
@@ -1,5 +1,31 @@
 <?php
 
+// code for getting image from TMDB:
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.themoviedb.org/3/movie/[TMDBID]/images?language=en-US&api_key=8058858125889a818b0e1831a9c045d0",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "{}",
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
 ?>
 
     <html>
