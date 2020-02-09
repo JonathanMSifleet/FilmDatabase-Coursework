585770f18340e16a4495734567d2210eead495ef
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-02-09 19:03:40 +0000

    Fix incoherent font

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ echo <<<_END
 	</head>
     <h1>Movie Database Presentation Tool</h1>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar">
   <a class="navbar-brand" href="home.php">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
