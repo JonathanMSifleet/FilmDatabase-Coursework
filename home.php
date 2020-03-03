7b8a66305eb7e227502a2ec5c0112e31ab94db33
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-03-03 15:16:36 +0000

    Remove unecessary heredocs

--- home.php
+++ home.php
@@ -1,45 +1,43 @@
 <?php
-
 require_once "header.php";
 require_once "helper.php";
+?>
 
-// main page
-echo <<<_END
-<title>MDPT</title>
+    <title>MDPT</title>
     <header class="homeHeader">
 
-        <div class="headertitle"> 
+        <div class="headertitle">
 
             <h1>Movie Database presentation</h1>
             <a href="#" class="btn js-scroll-to-searchmovies">Search Movie</a>
-        
-        </div> 
-       
+
+        </div>
+
     </header>
 
-    
+
     <section class="test">
         <div class="insideDiv">
             <div class="row2">
                 <div class="col2 span-1-of-22 norcol">
                     <h1>About the site</h1>
                     <p>Well come to the movie database presentation. This is a fully featured web app where you can browse and search content of the latest movies</p>
-                </div>   
+                </div>
                 <div class="col2 span-1-of-22 piccol">
                     <h1>Join the community</h1>
-                    
+
                     <a class="btn" href="">Login or Sign up</a>
-                    
-                </div>     
+
+                </div>
             </div>
         </div>
     </section>
-_END;
+
+<?php
 
 $query = "SELECT title, movie_id, release_date, rating FROM `movie` WHERE votes > 100 ORDER BY rating DESC LIMIT 8";
 $result = mysqli_query($connection, $query);
 $n = mysqli_num_rows($result);
-
 
 echo <<<_END
 <section>
@@ -67,31 +65,26 @@ for ($i = 0; $i < $n; $i++) {
     </div>
 
 _END;
-
-
 }
+?>
 
-echo <<<_END
-</div>
-</section>
+    </div>
+    </section>
 
-_END;
-
-echo <<<_END
     <section class="search_movie js--wp-1">
 
         <form action="view_movie.php?movieID=2">
-        <h1>Search Movie</h1>
+            <h1>Search Movie</h1>
 
-        <div class="container">
-            <input type="text" placeholder="Search...">
-            <div class="search"></div>
-        </div>
-    
-        <input class="btn" type="submit" value="Search">
-    </form> 
-    </section>  
-_END;
+            <div class="container">
+                <input type="text" placeholder="Search...">
+                <div class="search"></div>
+            </div>
+
+            <input class="btn" type="submit" value="Search">
+        </form>
+    </section>
+<?php
 
 $query = "SELECT * FROM `movie` order by RAND() LIMIT 8";
 $result = mysqli_query($connection, $query);
@@ -125,57 +118,51 @@ for ($i = 0; $i < $n; $i++) {
 _END;
 
 }
-echo <<<_END
-<br>
-</div>
-</section>
-_END;
 
-
-echo <<<_END
-<section>
-<form action="view_movie.php" method="POST" autocomplete="off" id="homeSearchMovie">
-    <label>Search a movie:</label>
-    <input type="text" name="title" id="search_movie" />
- 
-</form>
-
-<div class="col-md-5" style="position: relative; margin-top: 10px; margin-left:35px ">
-    <div class="list-group" id="show-list">
-   
+?>
+    <br>
     </div>
-</div>
+    </section>
+    <section>
+        <form action="view_movie.php" method="POST" autocomplete="off" id="homeSearchMovie">
+            <label>Search a movie:</label>
+            <input type="text" name="title" id="search_movie"/>
 
-</section>
-_END;
+        </form>
 
-echo <<<_END
-<script type="text/javascript">
+        <div class="col-md-5" style="position: relative; margin-top: 10px; margin-left:35px ">
+            <div class="list-group" id="show-list">
 
-$(document).ready(function(){
+            </div>
+        </div>
 
-    $("#search_movie").keyup(function(){
-        var searchText = $(this).val();
+    </section>
+    <script type="text/javascript">
 
-        if(searchText != ''){
-            $.ajax({
-                url:'autoc_movie.php',
-                method: 'GET',
-                data: {query: searchText},
-                success:function(response){
-                    $("#show-list").html(response);
+        $(document).ready(function () {
+
+            $("#search_movie").keyup(function () {
+                var searchText = $(this).val();
+
+                if (searchText != '') {
+                    $.ajax({
+                        url: 'autoc_movie.php',
+                        method: 'GET',
+                        data: {query: searchText},
+                        success: function (response) {
+                            $("#show-list").html(response);
+                        }
+
+                    });
+                } else {
+                    $("#show-list").html('');
                 }
-
             });
-        }
-        else{
-            $("#show-list").html('');
-        }
-    });    
-  
-});
-</script>
-_END;
+
+        });
+    </script>
+
+<?php
 
 require_once "footer.php";
 
